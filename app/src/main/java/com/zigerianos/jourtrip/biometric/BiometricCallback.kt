18cc0fbd1575a206ea8bd29f8fab890ac7501d9f
Cristian Espes <christian_zgza@hotmail.com>
2019-06-16 11:45:19 +0200

    Implementado paquete de Biometric-Auth

---
+++ app/src/main/java/com/zigerianos/jourtrip/biometric/BiometricCallback.kt
@@ -0,0 +1,26 @@
+package com.zigerianos.jourtrip.biometric
+
+interface BiometricCallback {
+
+    fun onSdkVersionNotSupported()
+
+    fun onBiometricAuthenticationNotSupported()
+
+    fun onBiometricAuthenticationNotAvailable()
+
+    fun onBiometricAuthenticationPermissionNotGranted()
+
+    fun onBiometricAuthenticationInternalError(error: String)
+
+
+    fun onAuthenticationFailed()
+
+    fun onAuthenticationCancelled()
+
+    fun onAuthenticationSuccessful()
+
+    fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence)
+
+    fun onAuthenticationError(errorCode: Int, errString: CharSequence)
+
+}
--- app/src/main/java/com/zigerianos/jourtrip/presentation/scenes/initial/InitialFragment.kt
+++ app/src/main/java/com/zigerianos/jourtrip/presentation/scenes/initial/InitialFragment.kt
@@ -8,6 +8,8 @@ import com.zigerianos.jourtrip.presentation.base.BaseFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_initial.*
 import org.koin.android.ext.android.inject
+import com.zigerianos.jourtrip.biometric.BiometricManager
+import com.zigerianos.jourtrip.biometric.BiometricCallback
 
 
 class InitialFragment : BaseFragment<IInitialPresenter.IInitialView, IInitialPresenter>(), IInitialPresenter.IInitialView {
@@ -31,7 +33,60 @@ class InitialFragment : BaseFragment<IInitialPresenter.IInitialView, IInitialPre
         progressBar.visibility = View.GONE
 
         buttonSignup.setOnClickListener { presenter.signupClicked() }
-        buttonExistingAccount.setOnClickListener { presenter.existingAccountClicked() }
+        buttonExistingAccount.setOnClickListener {
+
+            BiometricManager.BiometricBuilder(context!!)
+                .setTitle("Add a title")
+                .setSubtitle("Add a subtitle")
+                .setDescription("Add a description")
+                .setNegativeButtonText("Add a cancel button")
+                .build()
+                .authenticate(object : BiometricCallback {
+                    override fun onSdkVersionNotSupported() {
+                        println("Patata -> onSdkVersionNotSupported")
+                    }
+
+                    override fun onBiometricAuthenticationNotSupported() {
+                        println("Patata -> onBiometricAuthenticationNotSupported")
+                    }
+
+                    override fun onBiometricAuthenticationNotAvailable() {
+                        println("Patata -> onBiometricAuthenticationNotAvailable")
+                    }
+
+                    override fun onBiometricAuthenticationPermissionNotGranted() {
+                        println("Patata -> onBiometricAuthenticationPermissionNotGranted")
+                    }
+
+                    override fun onBiometricAuthenticationInternalError(error: String) {
+                        println("Patata -> onBiometricAuthenticationInternalError")
+                    }
+
+                    override fun onAuthenticationFailed() {
+                        println("Patata -> onAuthenticationFailed")
+                    }
+
+                    override fun onAuthenticationCancelled() {
+                        println("Patata -> onAuthenticationCancelled")
+                    }
+
+                    override fun onAuthenticationSuccessful() {
+                        println("Patata -> onAuthenticationSuccessful")
+                    }
+
+                    override fun onAuthenticationHelp(helpCode: Int, helpString: CharSequence) {
+                        println("Patata -> onAuthenticationHelp")
+                    }
+
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                        println("Patata -> onAuthenticationError")
+                    }
+
+                })
+
+
+            //presenter.existingAccountClicked()
+        }
     }
 
     override fun navigateToSignup() {
